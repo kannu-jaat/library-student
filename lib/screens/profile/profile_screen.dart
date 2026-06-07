f91d89a824ff5cda65f0a9951c7536f308f37943
kannu-jaat <krix7096@gmail.com>
2026-06-07 23:10:39 +0530

    Update profile_screen.dart

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,1 +1,28 @@
 // Profile screen
+
+import 'package:flutter/material.dart';
+import '../login/login_screen.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Profile')),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+          onPressed: () {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginScreen()),
+              (route) => false,
+            );
+          },
+          child: const Text('LOGOUT'),
+        ),
+      ),
+    );
+  }
+}
