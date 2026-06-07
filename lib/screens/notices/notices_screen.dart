bec98b18a660f317310a337cbc242f2fc0b0ecea
kannu-jaat <krix7096@gmail.com>
2026-06-07 23:10:10 +0530

    Update notices_screen.dart

--- lib/screens/notices/notices_screen.dart
+++ lib/screens/notices/notices_screen.dart
@@ -1,1 +1,17 @@
 // Notices screen
+
+import 'package:flutter/material.dart';
+
+class NoticesScreen extends StatelessWidget {
+  const NoticesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notices & Alerts')),
+      body: const Center(
+        child: Text('Admin Notices will be displayed here'),
+      ),
+    );
+  }
+}
