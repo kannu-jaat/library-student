e78d67842865cf05dcf8cba258e459bc0ee7f560
kannu-jaat <krix7096@gmail.com>
2026-06-07 23:09:02 +0530

    Update attendance_screen.dart

--- lib/screens/attendance/attendance_screen.dart
+++ lib/screens/attendance/attendance_screen.dart
@@ -1,1 +1,17 @@
 // Attendance screen
+
+import 'package:flutter/material.dart';
+
+class AttendanceScreen extends StatelessWidget {
+  const AttendanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mark Attendance')),
+      body: const Center(
+        child: Text('QR Scanner & Auto Selfie will come here'),
+      ),
+    );
+  }
+}
