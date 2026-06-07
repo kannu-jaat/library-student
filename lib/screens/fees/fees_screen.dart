796a72a33ef4f85f329d91b4a62567f87fc9bd00
kannu-jaat <krix7096@gmail.com>
2026-06-07 23:09:33 +0530

    Update fees_screen.dart

--- lib/screens/fees/fees_screen.dart
+++ lib/screens/fees/fees_screen.dart
@@ -1,1 +1,18 @@
 // Fees screen
+
+
+import 'package:flutter/material.dart';
+
+class FeesScreen extends StatelessWidget {
+  const FeesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Fee Status')),
+      body: const Center(
+        child: Text('UPI QR & Fee Payment History will come here'),
+      ),
+    );
+  }
+}
